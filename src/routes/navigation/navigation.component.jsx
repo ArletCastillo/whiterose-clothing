860a38cbc11f1833ca56bfeb5877e6b120d4d55e
ArletCastillo <arletmarcel24@gmail.com>
2023-07-13 19:55:56 -0400

    added logo svg

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,17 @@
 import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom"
 
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss'
+
 const Navigation = () => {
     return(
       <Fragment>
         <div className="navigation">
-            <Link className="logo" to="/"><div>Logo</div></Link>
+            <Link className="logo-container" to="/">
+              <CrwnLogo className="logo" />
+            </Link>
             <div className="nav-links-container">
                 <Link className="nav-link" to="/shop">SHOP</Link>
             </div>
