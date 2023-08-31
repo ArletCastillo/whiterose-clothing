4aee6733be3e051810714a7f360e7e038b637c33
ArletCastillo <arletmarcel24@gmail.com>
2023-08-31 19:59:59 -0400

    removed console.log

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -34,8 +34,7 @@ const SignInForm = () => {
         event.preventDefault();
 
         try {
-            const response = await signInAuthUserWithEmailAndPassword(email, password);
-            console.log(response);
+            await signInAuthUserWithEmailAndPassword(email, password);
             resetFormFields();
         } 
         catch(error) {
