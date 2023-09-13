2cbe6356f30db092d35ba64ca558e2df5dec714b
ArletCastillo <arletmarcel24@gmail.com>
2023-09-12 21:28:11 -0400

    created and leveraging ProductsContext

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,13 @@
-import SHOP_DATA from '../../shop-data.json';
+import { useContext } from "react";
+
+import { ProductContext } from "../../contexts/products.context";
 
 const Shop = () => {
+    const { products } = useContext(ProductContext);
+
     return (
         <div>
-            {SHOP_DATA.map(({id, name}) => (
+            {products.map(({id, name}) => (
                 <div key={id}>
                     <h2>{name}</h2>
                 </div>
