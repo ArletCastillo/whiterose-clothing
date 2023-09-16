7d13c0b6bd21f3f2788eaeeabb80bca7dbb5c591
ArletCastillo <arletmarcel24@gmail.com>
2023-09-16 13:33:11 -0400

    calling add items function from product component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,10 @@
+import './cart-dropdown.styles.scss';
+
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
-
-import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
     const {cartItems} = useContext(CartContext);
