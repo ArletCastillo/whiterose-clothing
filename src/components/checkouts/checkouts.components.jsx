e68f227e08bf6d9ee3854686ed175f9b93a42cb5
ArletCastillo <arletmarcel24@gmail.com>
2023-10-10 19:18:00 -0400

    created and leveraged CheckoutItem component

--- src/components/checkouts/checkouts.components.jsx
+++ src/components/checkouts/checkouts.components.jsx
@@ -3,6 +3,7 @@ import './checkouts.styles.scss';
 import { useContext } from 'react';
 
 import { CartContext } from '../../contexts/cart.context';
+import CheckoutItem from '../checkout-item/checkout-item.component';
 
 const Checkout = () => {
     const { cartItems, addItemToCart, removeItemFromCart } = useContext(CartContext);
@@ -26,19 +27,9 @@ const Checkout = () => {
                     <span>Remove</span>
                 </div>
             </div>
-            {cartItems.map(cartItem => {
-                const { id, name, quantity } = cartItem;
-                return (
-                    <div key={id}>
-                        <h2>{name}</h2>
-                        <span>{quantity}</span>
-                        <br/>
-                        <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
-                        <br/>
-                        <span onClick={() => addItemToCart(cartItem)}>increment</span>
-                    </div>
-                );
-            })}
+            {cartItems.map(cartItem =>
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            )}
             <span className='total'>Total: 0</span>
         </div>
     );
