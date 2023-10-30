7cc700661a21a80bf8e60efc4cd35f6a14baf6d1
ArletCastillo <arletmarcel24@gmail.com>
2023-10-30 18:31:04 -0400

    created and leveraging categoriesMap for shop data

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState, useEffect } from "react";
+
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+
+// import SHOP_DATA from '../shop-data';
+
+export const CategoriesContext = createContext({
+    categoriesMap: {}
+});
+
+export const CategoriesProvider = ({children}) => {
+    const [categoriesMap, setCategoriesMap] = useState({});
+    const value = {categoriesMap};
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+            setCategoriesMap(categoryMap);
+        }
+        getCategoriesMap();
+    }, []);
+
+    // used to post data to firestore, don't need it anymore
+    // useEffect(() => {
+    //     addCollectionAndDocuments('categories', SHOP_DATA);
+    // }, []);
+
+    return (
+        <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
+    );
+}
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -14,7 +14,9 @@ import {
   getDoc, 
   setDoc, 
   collection,
-  writeBatch 
+  writeBatch,
+  query,
+  getDocs
 } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -50,6 +52,20 @@ const firebaseConfig = {
 
     await batch.commit();
     console.log('done');
+  }
+
+  export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => { // acc => accumulator
+      const { title, items } = docSnapshot.data();
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+
+    return categoryMap;
   }
 
   export const createUserDocumentFromAuth = async (userauth) => {
