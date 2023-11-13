d7619cd99c4c28c397c9aca3eefa07ec9fce7dad
ArletCastillo <arletmarcel24@gmail.com>
2023-11-12 20:02:30 -0400

    Using Category routing in Shop component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
 import './shop.styles.scss';
 
@@ -8,6 +9,7 @@ const Shop = () => {
     return (
         <Routes>
             <Route index element={<CategoriesPreview />} /> {/*Parent route*/}
+            <Route path=':category' element={<Category />} /> {/*category is a parameter that is going to change */}
         </Routes>
     );
 }
