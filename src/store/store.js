e04ba250ebf3bf28371b8ebc32e16f8633b0faa8
ArletCastillo <arletmarcel24@gmail.com>
2024-02-20 12:08:33 -0400

    finished store file

--- src/store/store.js
+++ src/store/store.js
@@ -4,5 +4,16 @@
  */
 
 import { compose, legacy_createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import logger from 'redux-logger'; // optional
 
+import { rootReducer } from './root-reducer';
+
+const middleWares = [logger];
+const composedEnhacers = compose(applyMiddleware(...middleWares));
+
+/**
+ * First parameter: the root reducer
+ * Second parameter: extra state
+ * Third parameter: procedures to be executed before an action executes a reducer
+ */
+export const store = legacy_createStore(rootReducer, undefined, composedEnhacers);
