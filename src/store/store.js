e5d998156dcc289d39a2cc00032f8f62e11035dc
ArletCastillo <arletmarcel24@gmail.com>
2024-02-20 11:30:31 -0400

    added descriptive cooment to store.js file

--- src/store/store.js
+++ src/store/store.js
@@ -1,2 +1,8 @@
+/**
+ * File where all the Redux logic happens: where the state lives, actions are received and dispatched to reducers.
+ * Generates the object used for the application.
+ */
+
 import { compose, legacy_createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+
